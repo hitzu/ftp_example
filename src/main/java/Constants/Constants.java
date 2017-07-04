5932fb336df1d75502a2e93d2b05a0fb522eef81
Ivan&Liz <Ivan&Liz@DESKTOP-AT14V2I>
2017-07-04 10:42:55 -0500

    "Ip's"

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -14,19 +14,21 @@ public class Constants {
     public String nombre1 = "Hitzu";
     public String nombre2 = "Sandy";
     public String nombre3 = "Ivan";
-    //public String nombre4 = "Otro";
-    public String ip1 = "192.168.0.11";
-    public String ip2 = "192.168.0.13";
-    public String ip3 = "192.168.0.12";
-    //public String ip4 = "192.168.1.14";
+    public String nombre4 = "Karen";
+    public String ip1 = "10.250.20.75"; //Hitzu
+    public String ip2 = "192.168.0.13"; //Sandy
+    public String ip3 = "10.250.23.145"; //Ivan&Liz
+    public String ip4 = "10.250.0.125"; //karen
+    
     public String usr1 = "Hitzu";
     public String usr2 = "Sandy";
     public String usr3 = "Ivan&Liz";
-    //public String usr4 = "";
+    public String usr4 = "karen";
+    
     public String pass1 = "olagatito!";
     public String pass2 = "sandy";
     public String pass3 = "ihernandez";
-    //public String pass4 = "192.168.1.14";
+    public String pass4 = "liloflo";
     
     public String nombreLocal = "/Hitzu";
     public String localhost = "localhost";
